dad195a3af9868acc1fab3e24aea08385a7ec2ed
Cecimartinez <martinezcecilia2908@gmail.com>
2024-02-18 20:28:50 -0300

    feat: Search component and style enhancements

---
+++ gourmet-app/ui/components/Search.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, TextInput } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+export default function Search() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.iconContainer}>
+                <AntDesign name="search1" style={styles.icon} strokeWidth={3} color="gray" />
+            </View>
+            <TextInput
+                placeholder="Search.."
+                placeholderTextColor={'gray'}
+                style={styles.input}
+            />
+        </View>
+    );
+}
+
+const styles = {
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#E2E2E2',
+        borderRadius: 20, 
+        paddingVertical: 6,
+        paddingHorizontal: 10,
+    },
+    input: {
+        flex: 1,
+        fontSize: 20,
+        paddingLeft: 10,
+    },
+    iconContainer: {
+        borderRadius: 999, 
+        padding: 10,
+    },
+    icon: {
+        fontSize: 24,
+    },
+};
--- gourmet-app/ui/screens/HomeScreen.js
+++ gourmet-app/ui/screens/HomeScreen.js
@@ -1,10 +1,27 @@
+import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
+import Search from '../components/Search'
 
 export default function HomeScreen() {
     return (
-        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Text>HomeScreen</Text>
+        <View style={{flex: 1}}>
+            <StatusBar style='dark'/>
+            <ScrollView
+                style={{marginTop: 50}}
+                contentContainerStyle={{paddingHorizontal: '5%', paddingBottom: 50}}
+                showsVerticalScrollIndicator={false}
+            >
+                <View style={{marginBottom: 20}}>
+                    <Text style={{fontSize: 18, color: '#555', textAlign: 'left'}}>Hello, Ana!</Text>
+                    <View>
+                        <Text style={{fontSize: 25, fontWeight: 'bold', color: '#555', textAlign: 'left'}}>Make your own food,</Text>
+                    </View>
+                    <Text style={{fontSize: 25, fontWeight: 'bold', color: '#555', textAlign: 'left'}}>stay at <Text style={{color: '#ff9900'}}>home</Text></Text>
+                </View>
+
+                <Search/>
+            </ScrollView>
         </View>
     )
 }
