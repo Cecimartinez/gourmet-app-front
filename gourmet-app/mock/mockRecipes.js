fbe3ef7223244d4a6d75af09e39202dab7b49e43
Maximiliano <maxipk@gmail.com>
2024-02-19 18:19:48 -0300

    Pantalla de favoritos

--- gourmet-app/mock/mockRecipes.js
+++ gourmet-app/mock/mockRecipes.js
@@ -10,63 +10,76 @@ const categories = [
 
 export const mockRecipes = [
   {
-    img: "../assets/images/recipes/tarta-manzana.png", 
+    img: "https://s1.eestatic.com/2015/06/10/cocinillas/cocinillas_40006005_116187701_1706x960.jpg", 
     categoria: categories[3].name, // Dinner
     nombre: "Tarta de Manzana",
     descripcion: "Deliciosa tarta de manzana casera.",
     pasos: ["Pelar y cortar las manzanas.", "Preparar la masa.", "Hornear la tarta."],
     infoNutricional: { calorias: 300, proteinas: 5, carbohidratos: 50, grasas: 10 },
     videoYoutube: "https://www.youtube.com/watch?v=video1",
-    ingredientes: ["manzanas", "harina", "azúcar", "mantequilla"]
+    ingredientes: ["manzanas", "harina", "azúcar", "mantequilla"],
+    tiempo:["20 min"],
+    estrellas:["3"],
   },
   {
-    img: "../assets/images/recipes/pollo-curry.png",
+    img: "https://s1.eestatic.com/2015/06/10/cocinillas/cocinillas_40006005_116187701_1706x960.jpg",
     categoria: categories[3].name, // Dinner
     nombre: "Pollo al Curry",
     descripcion: "Pollo cocinado con una deliciosa salsa de curry.",
     pasos: ["Cocinar el pollo.", "Preparar la salsa de curry.", "Mezclar todo y servir."],
     infoNutricional: { calorias: 400, proteinas: 30, carbohidratos: 20, grasas: 15 },
     videoYoutube: "https://www.youtube.com/watch?v=video2",
-    ingredientes: ["pollo", "curry", "cebolla", "ajo", "crema"]
+    ingredientes: ["pollo", "curry", "cebolla", "ajo", "crema"],
+    tiempo:["30 min"],
+    estrellas:["4.5"],
   },
   {
-    img: "../assets/images/recipes/batido.png",
+    img: "https://s1.eestatic.com/2015/06/10/cocinillas/cocinillas_40006005_116187701_1706x960.jpg",
     categoria: categories[4].name, // Dessert
     nombre: "Batido de Frutas",
     descripcion: "Batido refrescante de frutas variadas.",
     pasos: ["Cortar las frutas.", "Licuar con hielo.", "Servir y disfrutar."],
     infoNutricional: { calorias: 200, proteinas: 3, carbohidratos: 40, grasas: 2 },
     videoYoutube: "https://www.youtube.com/watch?v=video3",
-    ingredientes: ["fresas", "plátano", "kiwi", "jugo de naranja"]
+    ingredientes: ["fresas", "plátano", "kiwi", "jugo de naranja"],
+    tiempo:["15 min"],
+    estrellas:["4.5"],
   },
   {
-    img: "../assets/images/recipes/ensalada-griega.png",
+    img: "https://s1.eestatic.com/2015/06/10/cocinillas/cocinillas_40006005_116187701_1706x960.jpg",
     categoria: categories[5].name, // Mediterranean
     nombre: "Ensalada Griega",
     descripcion: "Ensalada fresca con tomate, pepino, cebolla, queso feta y aceitunas.",
     pasos: ["Cortar los vegetales.", "Agregar el queso feta y las aceitunas.", "Aliñar con aceite de oliva y orégano."],
     infoNutricional: { calorias: 250, proteinas: 10, carbohidratos: 15, grasas: 18 },
     videoYoutube: "https://www.youtube.com/watch?v=video4",
-    ingredientes: ["tomate", "pepino", "cebolla", "queso feta", "aceitunas"]
+    ingredientes: ["tomate", "pepino", "cebolla", "queso feta", "aceitunas"],
+    tiempo:["330 min"],
+    estrellas:["4.5"],
   },
   {
-    img: "../assets/images/recipes/vegan-burger.png",
+    img: "https://s1.eestatic.com/2015/06/10/cocinillas/cocinillas_40006005_116187701_1706x960.jpg",
     categoria: categories[6].name, // Vegan
     nombre: "Hamburguesa Vegana",
     descripcion: "Hamburguesa hecha con garbanzos, zanahoria y especias.",
     pasos: ["Triturar los garbanzos y mezclar con zanahoria rallada y especias.", "Formar las hamburguesas y cocinar a la plancha.", "Servir en pan integral con vegetales frescos."],
     infoNutricional: { calorias: 320, proteinas: 12, carbohidratos: 45, grasas: 8 },
     videoYoutube: "https://www.youtube.com/watch?v=video5",
-    ingredientes: ["garbanzos", "zanahoria", "cebolla", "pan integral"]
+    ingredientes: ["garbanzos", "zanahoria", "cebolla", "pan integral"],
+    tiempo:["30 min"],
+    estrellas:["4"],
   },
   {
-    img: "../assets/images/recipes/pastel-chocolate.png",
+    img: "https://s1.eestatic.com/2015/06/10/cocinillas/cocinillas_40006005_116187701_1706x960.jpg",
     categoria: categories[4].name, // Dessert
     nombre: "Pastel de Chocolate",
     descripcion: "Pastel de chocolate esponjoso y decadente.",
     pasos: ["Preparar la masa de chocolate.", "Hornear el pastel.", "Decorar con crema batida y fresas."],
     infoNutricional: { calorias: 400, proteinas: 6, carbohidratos: 50, grasas: 20 },
     videoYoutube: "https://www.youtube.com/watch?v=video6",
-    ingredientes: ["harina", "azúcar", "chocolate", "huevos", "mantequilla"]
-  }
+    ingredientes: ["harina", "azúcar", "chocolate", "huevos", "mantequilla"],
+    tiempo:["30 min"],
+    estrellas:["4.5"],
+  },
+  
 ];
--- gourmet-app/ui/screens/FavoriteRecipesScreen.js
+++ gourmet-app/ui/screens/FavoriteRecipesScreen.js
@@ -1,10 +1,126 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React,{FC} from 'react';
+import { View, Text, FlatList, StyleSheet,Image } from 'react-native'; // Asegúrate de importar FlatList
+import { mockRecipes } from '../../mock/mockRecipes'; // Cambia la ruta según tu proyecto
+
 
 export default function FavoriteRecipesScreen() {
+    // Filtra las recetas favoritas (por ejemplo, si tienen la propiedad isFavorite establecida en true)
+    const favoriteRecipes = mockRecipes;
+     // Función para generar las estrellas
+  const renderStars = (numStars) => {
+    const stars = [];
+    for (let i = 0; i < numStars; i++) {
+      stars.push('⭐'); // Puedes usar cualquier otro ícono o estilo
+    }
+    return stars;
+  };
+  
     return (
-        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Text>FavoriteRecipesScreen</Text>
+        <View style={styles.container}>
+          <Text style={styles.title}>Recetas Favoritas</Text>
+          <FlatList data={favoriteRecipes}
+          keyExtractor={(item)=>item.nombre}
+          renderItem={({item}) =>(
+            <View style={styles.recipeItem}>
+               
+                <Image source={{uri:item.img}} style={styles.recipeImage}/>
+                <View style={styles.recipeInformation}>
+                    <Text style={styles.recipeName}>{item.nombre}</Text>
+                    <Text style={styles.recipeDescription}>{item.descripcion}</Text>
+                    <View style={styles.recipeIndicators}>
+                        <Image source={require('../../assets/icon-time.png')} style={styles.timeImage}/>
+                        <Text style={styles.recipeTime}>{item.tiempo}</Text>
+                        <Text style={styles.recipeStars}>{renderStars(item.estrellas)}</Text>
+                    </View>
+                </View>
+                
+
+            </View>
+          )}
+          />
+          
         </View>
-    )
-}
+      );
+    }
+
+
+const styles = StyleSheet.create({
+  container: {
+    
+    backgroundColor:'#fefefe',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding:20,
+    marginVertical:8,
+    marginHorizontal:12,
+    
+    
+  },
+recipeImage:{
+    height:150,
+    width:150,
+    alignItems:'center',
+  
+    borderRadius:12,
+    marginRight:14,
+
+},
+  title: {
+    color:'#FFA200',
+    fontSize: 15,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        marginLeft: 10,
+        alignSelf:'flex-start',
+        marginVertical:45,
+  },
+  recipeItem: {
+    
+    flexDirection: 'row',
+    fontSize:22,
+    flex:1,
+    backgroundColor:'rgb(249,247,248)',
+    padding: 16,
+    marginVertical:10,
+    borderRadius:24,
+    
+    padding:5,
+    width:'50%',
+  },
+  recipeName: {
+    
+    alignSelf:'flex-start',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color:'#000',
+  },
+  recipeDescription: {
+    
+    alignSelf:'flex-start',
+    fontSize: 16,
+     color:'#000',
+  },
+  recipeIndicators:{},
+
+  recipeTime:{
+
+    alignSelf:'flex-start',
+
+  },
+  recipeStars:{
+    alignSelf:'flex-start',
+
+
+  },
+  timeImage:{
+    marginTop:15,
+    width:30,
+    height:30,
+  },
+  recipeInformation:{
+    
+
+  },
+  
+});
