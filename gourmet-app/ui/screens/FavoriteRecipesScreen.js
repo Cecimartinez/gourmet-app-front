6a0ad48be78b065c5dc83a26f2a9a993a59028ee
maxipk <roisentulm@gmail.com>
2024-02-20 16:38:23 -0300

    Rediseño de favs

--- gourmet-app/ui/screens/FavoriteRecipesScreen.js
+++ gourmet-app/ui/screens/FavoriteRecipesScreen.js
@@ -1,50 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import { View, Text, FlatList, StyleSheet,Image,TouchableOpacity } from 'react-native'; // Asegúrate de importar FlatList
 import { mockRecipes } from '../../mock/mockRecipes'; // Cambia la ruta según tu proyecto
 import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 
 export default function FavoriteRecipesScreen() {
-    // Filtra las recetas favoritas (por ejemplo, si tienen la propiedad isFavorite establecida en true)
-    const favoriteRecipes = mockRecipes;
-     // Función para generar las estrellas
+
+    const [recipes, setRecipes] = useState([]); // Estado para almacenar las recetas
+   
+  
+    
 
   const navigation = useNavigation(); // Obtiene la función de navegación
-  const renderStars = (numStars) => {
-    
-    const stars = [];
-    for (let i = 0; i < numStars; i++) {
-      stars.push('⭐'); // Puedes usar cualquier otro ícono o estilo
-    }
-    return stars;
-  };
+  useEffect(() => {
+    // Realiza una solicitud HTTP para obtener las recetas desde tu backend
+    fetch('https://ad-backend-production.up.railway.app/api/recipes/')
+      .then((response) => response.json())
+      .then((data) => {
+        // Actualiza el estado con las recetas obtenidas
+        setRecipes(data);
+      })
+      .catch((error) => {
+        console.error('Error al obtener las recetas:', error);
+      });
+  }, []); // Se ejecuta solo una vez al montar el componente
+
+ 
   
     return (
         <View style={styles.container}>
           <Text style={styles.title}>Recetas Favoritas</Text>
-          <FlatList data={favoriteRecipes}
-          keyExtractor={(item)=>item.nombre}
+          
+          
+          <FlatList data={recipes}
+          keyExtractor={(item)=>item._id}
           renderItem={({item}) =>(
             
+            <View>
             <TouchableOpacity style={styles.recipeItem}
-            onPress={() => navigation.useNavigate("Receta" , { recipeId: item.id })} // Navega a la página de detalles
+            onPress={() => navigation.useNavigate("Receta" , { recipeId: item._id })} // Navega a la página de detalles
              >
                
-                <Image source={{uri:item.img}} style={styles.recipeImage}/>
+                <Image source={{uri:item.photo[0]}} style={styles.recipeImage}/>
                 <View style={styles.recipeInformation}>
-                    <Text style={styles.recipeName}>{item.nombre}</Text>
-                    <Text style={styles.recipeDescription}>{item.descripcion}</Text>
+                    <Text style={styles.recipeName}>{item.title}</Text>
+                    <Text style={styles.recipeDescription}>{item.description}</Text>
                     <View style={styles.recipeIndicators}>
-                      <View>
-                        <Image source={require('../../assets/icon-time.png')} style={styles.timeImage}/>
-                        <Text style={styles.recipeTime}>{item.tiempo}</Text>
-                      </View>
-                        <Text style={styles.recipeStars}>{renderStars(item.estrellas)}</Text>
+                      
+                      <MaterialCommunityIcons name="clock" size={24} color="black" style={styles.timeImage} />
+                      <Text style={styles.recipeTime}>{item.requiredTime}</Text>
+
+                  
+                     
+                     
+                      <MaterialIcons name="star-rate" size={24} color="gold" style={styles.recipeStarsIcon}/>
+                      <Text style={styles.recipeStars}>{item.rating}</Text>
+                      <FontAwesome name="bookmark" size={24} color="orange" style={styles.bookmark}/>
+                      
+                        
                     </View>
                 </View>
                 
 
             </TouchableOpacity>
+            </View>
           )}
           />
           
@@ -89,54 +111,77 @@ recipeImage:{
     flexDirection: 'row',
     fontSize:22,
     flex:1,
-    backgroundColor:'rgb(249,247,248)',
-    padding: 16,
-    marginVertical:10,
+    backgroundColor:'#fefefe',
+    padding: 5,
+    marginVertical:15,
     borderRadius:24,
+    marginRight:5,
     
-    padding:5,
     width:'50%',
   },
   recipeName: {
-    
+    margin:10,
     alignSelf:'flex-start',
     fontSize: 16,
     fontWeight: 'bold',
     color:'#000',
   },
+  
   recipeDescription: {
-    
+    marginLeft:5,    
     alignSelf:'flex-start',
     fontSize: 16,
      color:'#000',
   },
   recipeIndicators:{
     flexDirection: 'row',
+    justifyContent:'space-evenly',
+    marginBottom:5,
 
   },
 
   recipeTime:{
-    
-    marginTop:5,
-    marginLeft:5,
-    alignSelf:'flex-start',
+    fontSize:15,
+    fontWeight:'bold',
+  
+    marginTop:10,
+       alignSelf:'flex-start',
 
   },
   recipeStars:{
-    alignSelf:'flex-end',
+    fontSize:15,
+    fontWeight:'bold',
+    marginTop:10,
+    alignSelf:'flex-start',
     marginBottom:30,
+    marginRight:10,
+    
+
+  },recipeStarsIcon:{
+    marginTop:7,
+    alignSelf:'flex-start',
+    marginBottom:30,
+   
 
 
-  },
+  }
+  ,
   timeImage:{
-    marginTop:15,
+    marginTop:10,
     marginLeft:10,
     width:30,
     height:30,
   },
+  bookmark:{
+    marginTop:10,
+    alignSelf:'flex-start',
+    marginBottom:30,
+  },
   recipeInformation:{
     backgroundColor:'rgb(249,247,248)',
     borderRadius:24,
+    width:'100%',
+    
   
     
 
